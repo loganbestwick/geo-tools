fadd423bef0400be522936e8fafaeb9d2e888dc7
Logan Bestwick <loganbestwick@gmail.com>
2014-02-18 16:57:12 -0800

    google geocode API working

--- src/geotools.js
+++ src/geotools.js
@@ -1,19 +1,46 @@
+var http = require('http');
+
 var geoTools = {};
 
-geoTools.coorDist = function (lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d/1.60934;
+geoTools.distance = function (lat1, lon1, lat2, lon2) {
+	var R = 6371; // km
+	var dLat = toRad(lat2-lat1);
+	var dLon = toRad(lon2-lon1);
+	var lat1 = toRad(lat1);
+	var lat2 = toRad(lat2);
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
 }
 
-geoTools.haversine = function (lat1, lon1, lat2, lon2) {
-	
+function toRad(deg) {
+    /** Converts numeric degrees to radians */
+    return deg * Math.PI / 180;
 }
+
+geoTools.geoCode = function(address) {
+	var options = {
+		host : 'maps.googleapis.com',
+		path: '/maps/api/geocode/json?address=' + address.split(" ").join("+") + '&sensor=false'
+	}
+	var req = http.get(options, function(res){
+		console.log("Response: " + res.statusCode);
+		var results = '';
+		res.on('data', function(data){
+			results += data;
+		})
+		res.on('end', function() {
+			var body = JSON.parse(results)
+			var lat = (body.results[0].geometry.location.lat);
+			var lng = (body.results[0].geometry.location.lng);
+			console.log (lat, lng)
+		})
+	})
+}
+
+module.exports = geoTools;
+
+geoTools.geoCode('224 evergreen drive, kentfield, ca, 94904');
