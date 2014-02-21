881550071d2b71d59dc2fbc3ee6806492bc25ad0
Logan Bestwick <loganbestwick@gmail.com>
2014-02-20 17:38:05 -0800

    rgeocoding up and running

--- src/geotest.js
+++ src/geotest.js
@@ -7,18 +7,21 @@ var geoTools = require('./geotools.js');
 // console.log(toMeters(x));
 // console.log(toFeet(x));
 // console.log(toMiles(x));
-// geoCode('224 evergreen drive, kentfield, ca, 94904', function(coordinates){
+// geocode('224 evergreen drive, kentfield, ca, 94904', function(coordinates){
 // 	console.log(coordinates);
 // })
 
-// var y = geoCode('224 evergreen drive, kentfield, ca, 94904');
+// var y = geocode('224 evergreen drive, kentfield, ca, 94904');
+// console.log(y)
 
 // console.log(y);
 
-var p = {lat : 40.79162, lon : -111.765609}
-var m = {lat : 40.803544, lon : -111.773849}
+// var p = {lat : 40.79162, lon : -111.765609}
+// var m = {lat : 40.803544, lon : -111.773849}
 
-var x = distance(40.79162, -111.765609, 40.803544, -111.773849)
-var k = distance(p, m)
+// var x = distance(40.79162, -111.765609, 40.803544, -111.773849)
+// var k = distance(p, m)
 
-console.log(x, k)
+// console.log(x, k)
+
+reverseGeocode(37.944911, -122.55851)
--- src/geotools.js
+++ src/geotools.js
@@ -19,7 +19,7 @@ var url = require('url');
 		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
 		        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-		var d = R * c; // Distance in km
+		var d = R * c; // Distance in k
 		return d;
 	}
 
@@ -28,12 +28,12 @@ var url = require('url');
 	    return deg * Math.PI / 180;
 	}
 
-	geoCode = function(address, callback) {
-		//Prepares the inputted address into search query
+	geocode = function(address, callback) {
+		//Prepares the inputted address into the search query
 		var query = address.split(" ").join("+") + '&sensor=false'
 		var options = {
 			host : 'maps.googleapis.com',
-			path: '/maps/api/geocode/json?address=?' + query
+			path: '/maps/api/geocode/json?address=' + query
 			}
 			
 		var req = http.get(options, function(res){
@@ -47,10 +47,72 @@ var url = require('url');
 				var lat = (body.results[0].geometry.location.lat);
 				var lng = (body.results[0].geometry.location.lng);
 				var coordinates = {'lat' : lat, 'lng' : lng}
-				if (callback){ callback(coordinates)};
+				if (callback) {
+					callback(coordinates)
+				} else {
+					return coordinates
+				}
 			})
 		})
 	}
+
+	reverseGeocode = function(lat, lng, callback) {
+		//Prepares the inputted lat/lng into the search query
+		var query = lat.toString() + ',' + lng.toString() + '&sensor=false'
+		var options = {
+			host : 'maps.googleapis.com',
+			path: '/maps/api/geocode/json?latlng=' + query
+			}
+		var req = http.get(options, function(res){
+			console.log("Response: " + res.statusCode);
+			var results = '';
+			res.on('data', function(data){
+				results += data;
+			})
+			res.on('end', function() {
+				var body = JSON.parse(results)
+				var address = {
+					full_address : body.results[0].formatted_address
+				};
+				var addressJSON = body.results[0].address_components
+				for (var i = 0; i < addressJSON.length; i++) {
+					switch (addressJSON[i].types[0]) {
+						case 'street_number':
+							address.street_number = addressJSON[i].long_name
+							break;
+						case 'route':
+							address.street = addressJSON[i].long_name
+							break;
+						case 'neighborhood':
+							address.neighborhood = addressJSON[i].long_name
+							break;
+						case 'locality' || 'sublocality':
+							address.city = addressJSON[i].long_name
+							break;
+						case 'administrative_area_level_2':
+							address.county = addressJSON[i].long_name
+							break;
+						case 'administrative_area_level_1':
+							address.state = addressJSON[i].long_name
+							break;
+						case 'country':
+							address.country = addressJSON[i].long_name
+							break;
+						case 'postal_code':
+							address.postal_code = addressJSON[i].long_name
+							break;
+					}
+				}
+				if (callback) {
+					callback(address)
+				} else {
+					return address
+				}
+			})
+		})
+	}
+
+	
 
 	toMiles = function(distance) {
 		return distance * 1.60934
