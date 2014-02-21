91a150596b34d04df03f88eb7ee269200f0da68a
Logan Bestwick <loganbestwick@gmail.com>
2014-02-20 16:40:19 -0800

    input options solidified

--- src/geotest.js
+++ src/geotest.js
@@ -1,11 +1,24 @@
 var geoTools = require('./geotools.js');
-var y = geoCode('224 evergreen drive, kentfield, ca, 94904')
+
+// var x = distance(40.79162, -111.765609, 40.803544, -111.773849)
+
+// console.log(x);
+// console.log(toYards(x));
+// console.log(toMeters(x));
+// console.log(toFeet(x));
+// console.log(toMiles(x));
+// geoCode('224 evergreen drive, kentfield, ca, 94904', function(coordinates){
+// 	console.log(coordinates);
+// })
+
+// var y = geoCode('224 evergreen drive, kentfield, ca, 94904');
+
+// console.log(y);
+
+var p = {lat : 40.79162, lon : -111.765609}
+var m = {lat : 40.803544, lon : -111.773849}
+
 var x = distance(40.79162, -111.765609, 40.803544, -111.773849)
+var k = distance(p, m)
 
-console.log(x);
-console.log(toYards(x));
-console.log(toMeters(x));
-console.log(toFeet(x));
-console.log(toMiles(x));
-
-console.log(y);
+console.log(x, k)
--- src/geotools.js
+++ src/geotools.js
@@ -4,6 +4,12 @@ var url = require('url');
 (function() {
 
 	distance = function (lat1, lon1, lat2, lon2) {
+		if ((lat1.lat || lat1.latitude) && (lon1.lat || lon1.latitude)) {
+			lat2 = lon1.lat || lon1.latitude
+			lon2 = lon1.lon || lon1.longitude
+			lon1 = lat1.lon || lat1.longitude
+			lat1 = lat1.lat || lat1.latitude
+		}
 		var R = 6371; // km
 		var dLat = toRad(lat2-lat1);
 		var dLon = toRad(lon2-lon1);
@@ -22,7 +28,7 @@ var url = require('url');
 	    return deg * Math.PI / 180;
 	}
 
-	geoCode = function(address) {
+	geoCode = function(address, callback) {
 		//Prepares the inputted address into search query
 		var query = address.split(" ").join("+") + '&sensor=false'
 		var options = {
@@ -41,7 +47,7 @@ var url = require('url');
 				var lat = (body.results[0].geometry.location.lat);
 				var lng = (body.results[0].geometry.location.lng);
 				var coordinates = {'lat' : lat, 'lng' : lng}
-				return coordinates
+				if (callback){ callback(coordinates)};
 			})
 		})
 	}
